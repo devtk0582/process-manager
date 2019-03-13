361e7115013fc723a32f278459ac481d37a27a18
Ke Tang <jiushizhutk@gmail.com>
2019-03-12 22:05:22 -0400

    organized solution structure and added process host project

---
+++ src/processHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessManager.ProcessHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
